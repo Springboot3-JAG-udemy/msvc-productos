4cec81edad6bd87a1e20b2b275f048337fc9d444
AngelTravieso <angeltraviesoc@gmail.com>
2024-03-02 14:04:01 -0400

    Agregando clase entity Producto

---
+++ src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
@@ -0,0 +1,52 @@
+package com.formacionbdi.springboot.app.productos.models.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Double precio;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.DATE) // Formato en el que se guardara la fecha
+    private Date createAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
